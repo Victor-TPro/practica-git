76dc1c55ce2f61ca4397ae50465ebde28191bc79
Victor <rottenologic@gmail.com>
2025-11-24 19:07:38 +0100

    Ahora añado el archivo hellow.c de la Actividad 1 hecho en Eclipse

---
+++ hellow.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hellow.c
+ Author      : Victor
+ Version     : 1
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola, este es el final de la Actividad 1 de Fundamentos de Programación!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
